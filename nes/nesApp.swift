29fef08bda3c7e9e84457a859ca15682375b349c
Mike <mike@mike.town>
2025-07-27 13:12:12 -0400

    Initial Commit

---
+++ nes/nesApp.swift
@@ -0,0 +1,17 @@
+//
+//  nesApp.swift
+//  nes
+//
+//  Created by mike on 7/27/25.
+//
+
+import SwiftUI
+
+@main
+struct nesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
